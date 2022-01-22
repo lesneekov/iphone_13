65eec3cde2b5c0f8117de431c2576e33cd2792d8
lesneekov <95924829+lesneekov@users.noreply.github.com>
2022-01-22 16:08:40 +0700

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,32 @@
+const scrollFunc = () => {
+   const links = document.querySelectorAll('.header-menu__item a')
+   const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
+
+   const newAarray = [...links, linkCharacteristics]
+
+   seamless.polyfill()
+
+   newAarray.forEach((element) => {
+      element.addEventListener('click', (event) => {
+         event.preventDefault()
+
+         const id = element.getAttribute('href').substring(1)
+         const section = document.getElementById(id)
+
+         if (section) {
+            seamless.elementScrollIntoView(section, {
+               behavior: 'smooth',
+               block: 'start'
+            })
+         } else {
+            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+               behavior: "smooth",
+               block: "center",
+               inline: "center",
+            });
+         }
+
+      })
+   })
+}
+scrollFunc()
